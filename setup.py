b02b66e4b3cab78deb794aa807e7b3b3ce88072d
Danan Subramaniam <danan_varo@hotmail.com>
2025-10-15 20:45:49 +0200

    Remaining is READ.me

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import setup, find_packages
 setup(
     name="Schedule reminder",
     version="1.0",
-    packages=find_packages(),  # Includes the 'games' package
-    py_modules=['main'],       # Includes main.py as a top-level module
+    packages=find_packages(),  
+    py_modules=['main'],       
     include_package_data=True,
-    description="A fun game module as a part of ACIT4420 the lecture series. Basically from Lecture 5, 6, and 7.",
+    description="Mandatory assignment 2 for ACIT4420 Problem-solving with scripting",
     author="Danan Subramaniam",
     author_email="dasub6503@oslomet.no",
     install_requires=[],
     entry_points={
-        'console_scripts': [
-            'schedule-reminder=main:main',  # Points to main() in main.py (root directory)
+    "console_scripts": [
+            "schedule-reminder = main:main",  
         ],
     },
 )
